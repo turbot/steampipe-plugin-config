9915694bb82180f824aae4b69ef7a09a9e542387
Subhajit Kumar Mondal <subhajit@turbot.com>
2022-02-04 21:09:11 +0530

    Update parser and add docs

--- tables/table_json_key_value.go
+++ tables/table_json_key_value.go
@@ -2,14 +2,13 @@ package config
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"gopkg.in/yaml.v3"
 )
 
 func tableJSONKeyValue(ctx context.Context) *plugin.Table {
@@ -27,24 +26,23 @@ func tableJSONKeyValue(ctx context.Context) *plugin.Table {
 		},
 		Columns: []*plugin.Column{
 			{Name: "path", Type: proto.ColumnType_STRING, Description: "Specifies the path of the JSON file."},
-			{Name: "key", Type: proto.ColumnType_LTREE, Description: "Specifies full path of a key in JSON file."},
+			{Name: "key_path", Type: proto.ColumnType_LTREE, Transform: transform.FromField("Key").Transform(keysToSnakeCase), Description: "Specifies full path of a key in JSON file."},
 			{Name: "value", Type: proto.ColumnType_STRING, Description: "Specifies the value of the corresponding key."},
-			{Name: "keys", Type: proto.ColumnType_JSON, Description: "The array representation of path of a key."},
+			{Name: "keys", Type: proto.ColumnType_JSON, Transform: transform.FromField("Key"), Description: "The array representation of path of a key."},
 			{Name: "tag", Type: proto.ColumnType_STRING, Description: "Specifies the data type of the value."},
+			{Name: "start_line", Type: proto.ColumnType_INT, Description: "Specifies the line number where the value is located."},
+			{Name: "start_column", Type: proto.ColumnType_INT, Description: "Specifies the starting column of the value."},
 		},
 	}
 }
 
-type parseJSONFormat struct {
-	Path  string
-	Key   string
-	Value interface{}
-	Keys  []string
-	Tag   *string
-}
-
 func listJSONKeyValue(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Search for INI files to create as tables
+	// #1 - Path via qual
+	// If the path was requested through qualifier then match it exactly. Globs
+	// are not supported in this context since the output value for the column
+	// will never match the requested value.
+	//
+	// #2 - Path via glob paths in config
 	paths, err := fileList(ctx, d.Connection, ".json")
 	if err != nil {
 		return nil, err
@@ -55,73 +53,28 @@ func listJSONKeyValue(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrat
 	}
 
 	for _, path := range paths {
-		// Read file
-		jsonFile, err := os.Open(path)
+		// Read file content
+		reader, err := os.Open(path)
 		if err != nil {
-			return nil, fmt.Errorf("fail to read file: %v", err)
+			// Could not open the file, so log and ignore
+			plugin.Logger(ctx).Error("listJSONKeyValue", "file_error", err, "path", path)
+			return nil, nil
 		}
 
-		// defer the closing of jsonFile so that we can parse it later on
-		defer jsonFile.Close()
-
-		data := make(map[string]interface{})
-		byteValue, _ := ioutil.ReadAll(jsonFile)
-		err = json.Unmarshal([]byte(byteValue), &data)
+		var root yaml.Node
+		decoder := yaml.NewDecoder(reader)
+		err = decoder.Decode(&root)
 		if err != nil {
-			return nil, fmt.Errorf("fail to unmarshal data: %v", err)
+			plugin.Logger(ctx).Error("listJSONKeyValue", "parse_error", err, "path", path)
+			return nil, fmt.Errorf("failed to parse file %s: %v", path, err)
 		}
 
-		result := map[string]interface{}{}
-		for k, v := range data {
-			if IsArray(v) {
-				data := getNestedValue(k, v.([]interface{}), result)
-				for k, v := range data {
-					result[k] = v
-				}
-			} else if IsMap(v) {
-				for mapKey, mapValue := range v.(map[string]interface{}) {
-					newKey := fmt.Sprintf("%s.%s", k, mapKey)
-					getNestedValue(newKey, mapValue, result)
-				}
-			} else {
-				result[k] = v
-			}
-		}
-
-		for k, v := range result {
-			d.StreamListItem(ctx, parseJSONFormat{
-				Path:  path,
-				Key:   k,
-				Value: v,
-				Tag:   ValueToType(v),
-				Keys:  strings.Split(k, "."),
-			})
+		var rows Rows
+		treeToList(&root, []string{}, &rows, nil)
+		for _, r := range rows {
+			r.Path = path
+			d.StreamListItem(ctx, r)
 		}
 	}
 	return nil, nil
-}
-
-func getNestedValue(key string, value interface{}, result map[string]interface{}) map[string]interface{} {
-	if IsArray(value) { // check array
-		for count, i := range value.([]interface{}) {
-			if IsMap(i) { // check array<map>
-				for k, v := range i.(map[string]interface{}) {
-					newKey := fmt.Sprintf("%s.%d.%s", key, count, k)
-					getNestedValue(newKey, v, result)
-				}
-			} else {
-				newKey := fmt.Sprintf("%s.%d", key, count)
-				result[newKey] = i
-			}
-		}
-	} else if IsMap(value) { // check map
-		for k, v := range value.(map[string]interface{}) {
-			newKey := fmt.Sprintf("%s.%s", key, k)
-			getNestedValue(newKey, v, result)
-		}
-	} else {
-		result[key] = value
-	}
-
-	return result
 }
